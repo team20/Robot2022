89c1dcd416a8f2faecb34ce6ea777b8baa57fcc3
Emily Yin <46946140+yemily17@users.noreply.github.com>
2022-01-30 14:03:29 -0500

    Arm Stuff

--- src/main/java/frc/robot/subsystems/SlideArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/SlideArmSubsystem.java
@@ -1,11 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardContainer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ClimberSubsystem extends SubsystemBase {
+public class TelescopeArmSubsystem extends SubsystemBase{
   /** Creates a new ExampleSubsystem. */
-  public ClimberSubsystem() {}
+  public TelescopeArmSubsystem() {}
 
   @Override
   public void periodic() {
