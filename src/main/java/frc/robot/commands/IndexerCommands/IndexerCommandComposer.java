1f9f640756dca31f49d6160c3babd82520e212ea
23watejona <58493718+23watejona@users.noreply.github.com>
2022-02-11 20:26:28 -0500

    Format Document

--- src/main/java/frc/robot/commands/IndexerCommands/IndexerCommandComposer.java
+++ src/main/java/frc/robot/commands/IndexerCommands/IndexerCommandComposer.java
@@ -10,37 +10,45 @@ import frc.robot.subsystems.IndexerSubsystem;
 
 /** Add your docs here. */
 public class IndexerCommandComposer {
-     /**
-   * Based on the current indexer sensor states, decide what commands are needed in order to load a ball and return them
-   * @param indexerSubsystem
-   * @return command or command group to load a ball into the indexer
-   */
-  public static Command getLoadCommand(IndexerSubsystem indexerSubsystem){
-    if(indexerSubsystem.gamePieceRTS()){
-        return new SequentialCommandGroup(new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_REV), 
-                                          new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV));
-    }else{
-        return new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV);
+    /**
+     * Based on the current indexer sensor states, decide what commands are needed
+     * in order to load a ball and return them
+     * 
+     * @param indexerSubsystem
+     * @return command or command group to load a ball into the indexer
+     */
+    public static Command getLoadCommand(IndexerSubsystem indexerSubsystem) {
+        if (indexerSubsystem.gamePieceRTS()) {
+            return new SequentialCommandGroup(new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_REV),
+                    new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV));
+        } else {
+            return new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV);
+        }
     }
-}
-/**
- * Based on the current indexer sensor states, decide what commands are needed in order to shoot a ball and return them
- * NOTE: MUST BE USED IN SEQUENTIAL COMMAND GROUP AFTER FLYWHEEL SETTLE COMMAND **NO PARALLEL COMMAND GROUPS**
- * TODO: Add flywheel settle command so that we wait until flywheel is at setpoint before continuing
- * @param indexerSubsystem
- * @param flywheelSubsystem
- * @return command or command group to shoot a ball on the indexer side of things
- */
-public static Command getShootCommand(IndexerSubsystem indexerSubsystem){
-    if(indexerSubsystem.gamePieceRTS()){
-        return new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV);
-    }else if(indexerSubsystem.gamePieceAtCenter()){
-        return new SequentialCommandGroup(new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV), 
-                                            new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV));
-    }else{
-        return new SequentialCommandGroup(new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV), 
-                                            new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV), 
-                                            new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV));
+
+    /**
+     * Based on the current indexer sensor states, decide what commands are needed
+     * in order to shoot a ball and return them
+     * NOTE: MUST BE USED IN SEQUENTIAL COMMAND GROUP AFTER FLYWHEEL SETTLE COMMAND
+     * **NO PARALLEL COMMAND GROUPS**
+     * TODO: Add flywheel settle command so that we wait until flywheel is at
+     * setpoint before continuing
+     * 
+     * @param indexerSubsystem
+     * @param flywheelSubsystem
+     * @return command or command group to shoot a ball on the indexer side of
+     *         things
+     */
+    public static Command getShootCommand(IndexerSubsystem indexerSubsystem) {
+        if (indexerSubsystem.gamePieceRTS()) {
+            return new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV);
+        } else if (indexerSubsystem.gamePieceAtCenter()) {
+            return new SequentialCommandGroup(new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV),
+                    new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV));
+        } else {
+            return new SequentialCommandGroup(new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV),
+                    new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV),
+                    new IndexerCommand(indexerSubsystem, IndexerCommand.Operation.CMD_ADV));
+        }
     }
-}
 }
