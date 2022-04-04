2525a907d94ed526ef78ef9a8f314aeb79c6e8f8
20LM22 <etoile22lm@gmail.com>
2022-04-04 15:23:51 -0400

    Added usb camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -26,6 +27,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    CameraServer.startAutomaticCapture();
     m_robotContainer = new RobotContainer();
     LiveWindow.disableAllTelemetry();
   }
